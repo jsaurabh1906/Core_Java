4713efaf8a8f146f214275bfb20d5156aea2bc44
jsaurabh1906 <jsaurabh1906@gmail.com>
2023-06-21 22:29:25 +0530

    Problem on Strings update-1

--- Programs On Strings/src/Main.java
+++ Programs On Strings/src/Main.java
@@ -7,25 +7,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first String: ");
         String str1 = sc.nextLine();
-        System.out.println("Enter the second String: ");
-        String str2 = sc.nextLine();
+        //System.out.println("Enter the second String: ");
+        //String str2 = sc.nextLine();
         StringOperations so = new StringOperations();
 
         //System.out.println(so.removeSpaces3(str1));
+        //System.out.println(so.reverseString(str1));
         //System.out.println(so.reverseString2(str1));
         //System.out.println(so.toMyLowerCase(str1));
         //System.out.println(so.toMyUpperCase(str1));
         //System.out.println(so.sortASC(str1));
         //System.out.println(so.sortDESC(str1));
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /*          //check Anagrams
-        if(so.areAnagrams(str1,str2)){
-            System.out.println("Strings are Anagrams");
-        }
-        else {
-            System.out.println("Strings are Not Anagrams");
-        }*/
+        if(so.areAnagrams(str1,str2)) System.out.println("Strings are Anagrams");
+        else System.out.println("Strings are Not Anagrams");
+            */
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          /*       //check Pangrams
+        if(so.isPangram(str1)) System.out.println("String is Pangram");
+        else  System.out.println("String is Not Pangram");
+        */
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //so.isPalindrome(str1);
 
-
-
+        /*  if(so.isPalindrome2(str1)) System.out.println("Palindrome");
+            else System.out.println("not palindrome");
+        */
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //System.out.println(so.countChars2(str1));
+        //System.out.println(so.countChars3(str1));
+        //System.out.println(so.countChars(str1));
+        //System.out.println(so.sumOfNumbersInString(str1));
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // remove duplicates
+            //so.removeDuplicateChars(str1);
+            //so.removeDuplicateChars2(str1);
+            //so.removeDuplicateChars3(str1);
+            //so.removeDuplicateChars4(str1);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //so.countCharsOccurance(str1);
+        //so.countCharsInEachWord(str1);
+        //so.countWords(str1);
+        so.countAllSeparately(str1);
     }
 }
--- Programs On Strings/src/StringOperations.java
+++ Programs On Strings/src/StringOperations.java
@@ -1,3 +1,8 @@
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class StringOperations {
     String removeSpaces(String str){
         char ch[]=str.toCharArray();
@@ -35,9 +40,9 @@ public class StringOperations {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString();
     }
-    public String reverseString3(String str){
+    /*public String reverseString3(String str){
         return "";
-    }
+    }*/
     public String toMyLowerCase(String str){
         char []ch = str.toCharArray();
         String newStr="";
@@ -123,9 +128,201 @@ public class StringOperations {
    /* A pangram is a sentence containing every letter in the English Alphabet.*/
     public boolean isPangram(String str){
         str = removeSpaces3(str);
+        str = toMyLowerCase(str);
         int n=26;
         if(str.length() < n) return false;
-        
+        for (char c = 'a'; c <= 'z' ; c++) {
+            if (str.indexOf(c) < 0)
+                return false;
+        }
         return true;
     }
+    public void isPalindrome(String str) {
+        char ch[] = str.toCharArray();
+        for (int i = 0; i < ch.length / 2; i++) {
+            char temp = ch[i];
+            ch[i] = ch[ch.length-1-i];
+            ch[ch.length-1-i] = temp;
+        }
+        String newStr = new String(ch);
+
+        if(newStr.equalsIgnoreCase(str)){
+            System.out.println("String is Palindrome");
+        }
+        else{
+            System.out.println("String is not Palindrome");
+        }
+    }
+    public boolean isPalindrome2(String str){
+        String newStr = str.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        int left = 0;
+        int right = newStr.length()-1;
+        while (left < right){
+            if(newStr.charAt(left) != newStr.charAt(right)){
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+    public int countChars(String str){
+        char ch[] = str.toCharArray();
+        int charCount = 0;
+        for (char c : ch) {
+            if(c >= 'A' && c <= 'Z' ||                  //65-90
+               c >= 'a' && c <= 'z' ||                  //97-122
+               c >= '0' && c <= '9' &&                  //48-57
+               c != ',' && c !='.'  && c != ' ')        // space= 32
+            {charCount++;}
+        }
+        return charCount;
+    }
+    public int countChars2(String str){
+        return str.length();
+    }
+    public int countChars3(String str){
+        char ch[] = str.toCharArray();
+        int charCount = 0;
+        for (char c : ch) {
+            charCount++;
+        }
+        return charCount;
+    }
+    //to find the sum of numbers in an ALPHA NUMERIC STRING
+    public int sumOfNumbersInString(String str){
+        char ch[] = str.toCharArray();
+        int sumOfNums = 0;
+        for ( char c : ch) {
+            if (c >= '0' && c <= '9'){
+                sumOfNums += (c - '0');
+            }
+        }
+        return sumOfNums;
+    }
+
+    //remove duplicates in a string
+    //1. using java 8 feature
+    public void removeDuplicateChars(String str){
+        StringBuilder sb= new StringBuilder();
+        str.chars().distinct().forEach( c -> sb.append((char) c));
+        System.out.println(sb);
+    }
+    //2. using indexOf
+    public void removeDuplicateChars2(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length() ; i++) {
+            char ch = str.charAt(i);
+            int index = str.indexOf(ch, i+1 );  //search and return index of first occurence of ch from i+1 position if not returns -1
+            //if index = -1 means ch is not repeated
+            if(index == -1){
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb);
+    }
+    //3. using charArray
+    public void removeDuplicateChars3(String str) {
+        str = toMyLowerCase(str);
+        StringBuilder sb =new StringBuilder();
+        char ch[] = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            boolean repeated = false;
+            for (int j = i+1; j < ch.length; j++) {
+                if(ch[i] == ch[j]) {
+                    repeated = true;
+                    break;
+                }
+            }
+            if (!repeated){
+                sb.append(ch[i]);
+            }
+        }
+        System.out.println(sb);
+
+    }
+    //4. using set interface
+    public void removeDuplicateChars4(String str) {
+        StringBuilder sb = new StringBuilder();
+        Set<Character> charSet = new LinkedHashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            charSet.add(str.charAt(i));
+        }
+        for (char c : charSet) {
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+    public void countCharsOccurance(String str){
+        Map<Character, Integer> omap = new LinkedHashMap<>();
+        char []ch = str.toCharArray();
+        for(char c : ch){
+            if(!omap.containsKey(c))
+                omap.put(c,1);
+            else{
+                int count = omap.get(c);
+                omap.put(c, count+1);
+            }
+        }
+        System.out.println(omap);
+        for(Map.Entry<Character, Integer> ele : omap.entrySet()){
+            char key = ele.getKey();
+            int value = ele.getValue();
+            System.out.println(key +"...."+value);
+        }
+    }
+    public void countCharsInEachWord(String str){
+        for (int i = 0; i < str.length(); i++) {
+            String newStr = "";
+            while( i < str.length()  && str.charAt(i) != ' '){
+                newStr += str.charAt(i);
+                i++;
+            }
+            if (newStr.length() > 0){
+                System.out.println(newStr+"..."+newStr.length());
+            }
+        }
+    }
+    public void countWords(String str){
+        int wordCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' || i == str.length()-1){
+                wordCount++;
+            }
+        }
+        System.out.println(wordCount);
+    }
+    //number of LOWERCASE, UPPERCASE,SPECIAL SYMBOLS, SPACES and DIGITS in a STRING
+    public void countAllSeparately(String str){
+        char []ch = str.toCharArray();
+        int ucCount = 0;
+        int lcCount = 0;
+        int spacesCount = 0;
+        int spSymbolCount = 0;
+        int digitsCount = 0;
+        for (char c: ch) {
+            if (c >= 'A' && c <= 'Z') ucCount++ ;
+            else if (c >= 'a' && c <= 'z') lcCount++ ;
+            else if (c >= '0' && c <= '9') digitsCount++ ;
+            else if (c == ' ') spacesCount++ ;
+            else spSymbolCount++;
+        }
+        System.out.println("No. of upper-case characters: " + ucCount );
+        System.out.println("No. of lower-case characters: " + lcCount );
+        System.out.println("No. of digit characters: " + digitsCount);
+        System.out.println("No. of spaces characters: " + spacesCount );
+        System.out.println("No. of Special-symbol characters: " + spSymbolCount );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
